ffc7e4189e17925e986e021d7aebe54545792345
learn2412 <surya24.official@gmail.com>
2025-01-21 12:16:02 +0000

    comit frontend files

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [input, setInput] = useState('');
+
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleEvaluate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch {
+      setInput('Error');
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="text" value={input} readOnly />
+      </div>
+      <div>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={handleClear}>C</button>
+        <button onClick={() => handleClick('.')}>.</button>
+        <button onClick={handleEvaluate}>=</button>
+        <button onClick={() => handleClick('/')}>/</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
